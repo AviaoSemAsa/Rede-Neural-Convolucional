f616f31098ce5ae35e181684d6ddebf532e8669d
Gustavo Machado de Almeida <gu.machado1221@gmail.com>
2024-02-23 09:03:08 -0300

    path correction

--- CNN.py
+++ CNN.py
@@ -10,12 +10,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                       shear_range = 0.2,
                                       zoom_range = 0.2,
                                       horizontal_flip = True)
-training_set = train_datagen.flow_from_directory('C:/Users/L04693/Pictures/Section+40+-+Convolutional+Neural+Networks+(CNN)/Section 40 - Convolutional Neural Networks (CNN)/dataset/training_set',
+training_set = train_datagen.flow_from_directory('path/dataset/train_set',
                                                     target_size = (64, 64),
                                                     batch_size = 32,
                                                     class_mode = 'binary')
 test_datagen = ImageDataGenerator(rescale = 1./255)
-test_set = test_datagen.flow_from_directory('C:/Users/L04693/Pictures/Section+40+-+Convolutional+Neural+Networks+(CNN)/Section 40 - Convolutional Neural Networks (CNN)/dataset/test_set',
+test_set = test_datagen.flow_from_directory('path/dataset/train_set',
                                                 target_size = (64, 64),
                                                 batch_size = 32,
                                                 class_mode = 'binary')
